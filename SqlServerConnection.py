ae06fc05cc5c8cefeb7430cd986fd8e25f4d2f3d
brent-ban001 <brent.anthuenis@azstlucas.be>
2024-10-17 15:39:20 +0200

    addes code to communicate with the sql queue

---
+++ SqlServerConnection.py
@@ -0,0 +1,43 @@
+import configparser
+import pyodbc
+from Models import *
+
+class SqlServerConnection:
+    def __init__(self)-> None:
+        #get needed config data from ini file and create a new connection 
+        config = configparser.ConfigParser()
+        config.read(".ini")
+        dbconfiguration=config["queue_db"]
+        server = dbconfiguration["server"]
+        database = dbconfiguration["database"]
+        username = dbconfiguration["username"]
+        password = dbconfiguration["password"]
+        connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+        connection = pyodbc.connect(connection_string)
+        self.connection=connection
+
+    def getFirstQueueItem(self) -> TaskQueue:
+        statusToUse="pending"
+        query="""SELECT Top 1 id, task_type, payload, status, statuslog, retries, priority, created_at, updated_at, processed_at 
+            FROM tasks_queue
+            WHERE Status = ?"""
+        cursor = self.connection.cursor()
+        cursor.execute(query,statusToUse)
+        tasks = []
+        for row in cursor.fetchall():
+            task = TaskQueue(
+                id=row.id,
+                task_type=row.task_type,
+                payload=row.payload,  # Assuming payload is a JSON string or dict
+                status=row.status,
+                statuslog=row.statuslog,
+                retries=row.retries,
+                priority=row.priority,
+                created_at=row.created_at,
+                updated_at=row.updated_at,
+                processed_at=row.processed_at
+            )
+            tasks.append(task)
+        print("gevonden tasks:",tasks)
+        cursor.close()
+        return tasks[0]
