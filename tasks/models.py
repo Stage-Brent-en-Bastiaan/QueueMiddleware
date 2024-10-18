beb004005e8fe9f70c978e4d0423e2551084b2c9
bbasti16 <125884986+bbasti16@users.noreply.github.com>
2024-10-18 12:50:36 +0200

    database aangemaakt

---
+++ tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    task_type = models.CharField(max_length=25)
+    payload = models.TextField()
+    status = models.CharField(max_length=20)
+    statuslog = models.CharField(max_length=255)
+    retries = models.IntegerField()
+    priority = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    processed_at = models.DateTimeField(null=True, blank=True)
