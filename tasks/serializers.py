30b543d703f15801cd9c53bef8e396aa4e1e4f2f
bbasti16 <125884986+bbasti16@users.noreply.github.com>
2024-10-24 13:26:29 +0200

    GetPatients REST toegevoegd

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -20,32 +20,32 @@ class TaskSerializer(serializers.ModelSerializer):
         )
 
 
-class PatientSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    address = serializers.CharField()
-    box_code = serializers.CharField()
-    date_of_birth = serializers.DateField()
-    first_name = serializers.CharField()
-    gender = serializers.CharField()
-    gov_id = serializers.CharField()
-    hospital_id = serializers.CharField()
-    last_name = serializers.CharField()
-    locale = serializers.CharField()
-    nationality = serializers.CharField()
-    phone_contact = serializers.CharField()
+# class PatientSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     address = serializers.CharField()
+#     box_code = serializers.CharField()
+#     date_of_birth = serializers.DateField()
+#     first_name = serializers.CharField()
+#     gender = serializers.CharField()
+#     gov_id = serializers.CharField()
+#     hospital_id = serializers.CharField()
+#     last_name = serializers.CharField()
+#     locale = serializers.CharField()
+#     nationality = serializers.CharField()
+#     phone_contact = serializers.CharField()
 
-    def read(self, instance):
-        return {
-            "address": null,
-            "box_code": "mxjc1xe987irze64",
-            "date_of_birth": "1986-08-08",
-            "first_name": "Bob",
-            "gender": "male",
-            "gov_id": "11320079431",
-            "hospital_id": null,
-            "id": 394,
-            "last_name": "Marly",
-            "locale": null,
-            "nationality": null,
-            "phone_contact": null,
-        }
+#     def read(self, instance):
+#         return {
+#             "address": null,
+#             "box_code": "mxjc1xe987irze64",
+#             "date_of_birth": "1986-08-08",
+#             "first_name": "Bob",
+#             "gender": "male",
+#             "gov_id": "11320079431",
+#             "hospital_id": null,
+#             "id": 394,
+#             "last_name": "Marly",
+#             "locale": null,
+#             "nationality": null,
+#             "phone_contact": null,
+#         }
--- tests.py
+++ tests.py
@@ -5,3 +5,4 @@ from ApiCommunication.Patienten import Patienten
 # Create your tests here.
 patientenFactory: Patienten = Patienten()
 print(patientenFactory.getPatienten("?first_name=Bastiaan"))
+print(patientenFactory.getPatienten(""))
